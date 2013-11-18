e239ca583e6e01540ce0494bbed9b095c81788bb
Sibi <sibi@psibi.in>
2013-11-18 14:13:03 +0530

    Added more functions.

--- chapter 10/Parse.hs
+++ chapter 10/Parse.hs
@@ -2,6 +2,7 @@ import qualified Data.ByteString.Lazy.Char8 as L8
 import qualified Data.ByteString.Lazy as L
 import Data.Char (isSpace)
 import Data.Int (Int64)
+import Data.Word (Word8)
 
 data ParseState = ParseState {
   string :: L.ByteString,
@@ -27,4 +28,39 @@ parse parser initState
         Left err          -> Left err
         Right (result, _) -> Right result
        
-  
+modifyOffset :: ParseState -> Int64 -> ParseState
+modifyOffset initState newOffset =
+  initState {offset = newOffset}
+
+getState :: Parse ParseState
+getState = Parse (\s -> Right (s, s))
+
+putState :: ParseState -> Parse ()
+putState s = Parse (\_ -> Right((), s))
+
+(==>) :: Parse a -> (a -> Parse b) -> Parse b
+firstParser ==> secondParser  =  Parse chainedParser
+  where chainedParser initState   =
+          case runParse firstParser initState of
+            Left errMessage ->
+                Left errMessage
+            Right (firstResult, newState) ->
+                runParse (secondParser firstResult) newState
+
+
+bail :: String -> Parse a
+bail err = Parse $ \s -> Left $
+           "byte offset " ++ show (offset s) ++ ": " ++ err
+
+parseByte :: Parse Word8
+parseByte =
+  getState ==> \initState ->
+  case L.uncons (string initState) of
+    Nothing ->
+      bail "no more input"
+    Just (byte, remainder) ->
+      putState newState ==> \_ ->
+      identity byte
+      where newState = initState {string = remainder,
+                                  offset = newOffset}
+            newOffset = offset initState + 1
