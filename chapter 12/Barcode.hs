b85e20cd06b6c1fd5a9be1abb8d745a381a85690
Sibi <sibi@psibi.in>
2013-11-23 00:37:34 +0530

    Added more functions.

--- chapter 12/Barcode.hs
+++ chapter 12/Barcode.hs
@@ -1,2 +1,29 @@
+import Data.Array (Array(..), (!), bounds, elems, indices,
+                   ixmap, listArray)
+
+import Control.Applicative ((<$>))
+import Control.Monad (forM_)
+import Data.Char (digitToInt)
+import Data.Ix (Ix(..))
+import Data.List (foldl', group, sort, sortBy, tails)
+import Data.Maybe (catMaybes, listToMaybe)
+import Data.Ratio (Ratio)
+import Data.Word (Word8)
+import System.Environment (getArgs)
+import qualified Data.ByteString.Lazy.Char8 as L
+import qualified Data.Map as M
+
+checkDigit :: (Integral a) => [a] -> a
+checkDigit ds = 10 - (sum products `mod` 10)
+  where products = mapEveryOther (*3) (reverse ds)
+
+
 mapEveryOther :: (a -> a) -> [a] -> [a]
 mapEveryOther f = zipWith ($) (cycle [f, id])
+
+leftOddList = ["0001101", "0011001", "0010011", "0111101", "0100011",
+               "0110001", "0101111", "0111011", "0110111", "0001011"]
+
+rightList = map complement <$> leftOddList
+  where complement '0' = '1'
+        complement '1' = '0'
