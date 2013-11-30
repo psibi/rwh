f0f0b599dfb71ebf855da95a96b880e5286aba33
Sibi <sibi@psibi.in>
2013-11-30 23:26:56 +0530

    Added guarded function.

--- chapter 14/Logger.hs
+++ chapter 14/Logger.hs
@@ -11,4 +11,20 @@ instance Monad Logger where
                 (b, x) = execLogger n
             in Logger (b, w ++ x)
 
+record :: String -> Logger ()
+record s = Logger ((), [s])
+
+-- Sample List monad demo
+
+guarded :: Bool -> [a] -> [a]
+guarded True xs = xs
+guarded False _ = []
+
+
+multiplyTo :: Int -> [(Int, Int)]
+multiplyTo n = do
+  x <- [1..n]
+  y <- [1..n]
+  guarded (x * y == n) $
+    return (x, y)
 
