3a91cc03ab8b5ec15efbdc77bdb832e826e147cf
Sibi <sibi@psibi.in>
2013-11-30 20:47:17 +0530

    MovieReview.hs

---
+++ chapter 15/MovieReview.hs
@@ -0,0 +1,35 @@
+import Control.Monad (liftM3)
+
+data MovieReview = MovieReview {
+    revTitle :: String
+  , revUser :: String
+  , revReview :: String
+  }
+
+simpleReview :: [(String, Maybe String)] -> Maybe MovieReview
+simpleReview alist =
+  case lookup "title" alist of
+    Just (Just title@(_:_)) ->
+      case lookup "user" alist of
+        Just (Just user@(_:_)) ->
+          case lookup "review" alist of
+            Just (Just review@(_:_)) ->
+              Just (MovieReview title user review)
+            _ -> Nothing
+        _ -> Nothing
+    _ -> Nothing
+
+maybeReview :: [(String, Maybe String)] -> Maybe MovieReview
+maybeReview alist = do
+  title <- lookup1 "title" alist
+  user <- lookup1 "user" alist
+  review <- lookup1 "review" alist
+  return (MovieReview title user review)
+
+lookup1 key alist = case lookup key alist of
+                      Just (Just s@(_:_)) -> Just s
+                      _ -> Nothing
+
+liftedReview :: [(String, Maybe String)] -> Maybe MovieReview
+liftedReview alist = liftM3 MovieReview (lookup1 "title" alist)
+                     (lookup1 "user" alist) (lookup1 "review" alist)
