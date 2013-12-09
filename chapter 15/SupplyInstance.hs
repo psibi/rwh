2ce67fda5bacd32ba1f8fbb9e2be440d78f20345
Sibi <sibi@psibi.in>
2013-12-10 00:39:25 +0530

    MonadSupply

--- chapter 15/SupplyInstance.hs
+++ chapter 15/SupplyInstance.hs
@@ -1,3 +1,10 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE FlexibleInstances #-}
+
+import Control.Monad (liftM)
+
 newtype Reader e a = R { runReader :: e -> a }
 
 instance Monad (Reader e) where
@@ -6,3 +13,12 @@ instance Monad (Reader e) where
 
 ask :: Reader e e
 ask = R id
+
+newtype MySupply e a = MySupply { runMySupply :: Reader e a }
+                     deriving (Monad)
+
+class (Monad m) => MonadSupply s m | m -> s where
+  next :: m (Maybe s)
+
+instance MonadSupply s (MySupply e) where
+  next = MySupply (Just `liftM` ask)
