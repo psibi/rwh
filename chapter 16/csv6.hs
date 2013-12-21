c8521d7b1ef78a7c61327badf9aa90e41901d916
Sibi <sibi@psibi.in>
2013-12-21 15:54:05 +0530

    csv Parsing

---
+++ chapter 16/csv6.hs
@@ -0,0 +1,14 @@
+import Text.ParserCombinators.Parsec
+
+csvFile = endBy line eol
+line = sepBy cells (char ',')
+cells = many (noneOf ",\n\r")
+
+eol = try (string "\n\r")
+      <|> try (string "\r\n")
+      <|> string "\n"
+      <|> string "\r"
+
+parseCSV :: String -> Either ParseError [[String]]
+parseCSV input = parse csvFile "(unkown)" input
+
