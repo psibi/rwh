eafb01433ce119cdf74c3cc4cc63d0479cb39da3
Sibi <sibi@psibi.in>
2013-12-27 03:07:30 +0530

    eval2 Function.

--- transformers/Eval2.hs
+++ transformers/Eval2.hs
@@ -61,3 +61,22 @@ eval2c env (Abs n e) = return $ FunVal env n e
 eval2c env (App e1 e2) = do FunVal env' n body <- eval2c env e1
                             val2 <- eval2c env e2
                             eval2c (Map.insert n val2 env') body
+
+eval2 :: Env -> Exp -> Eval2 Value
+eval2 env (Lit i) = return $ IntVal i
+eval2 env (Var n) = case Map.lookup n env of
+                      Nothing -> throwError ("unbound variable: " ++ n)
+eval2 env (Plus e1 e2) = do e1' <- eval2 env e1
+                            e2' <- eval2 env e2
+                            case (e1', e2') of
+                              (IntVal i1, IntVal i2) ->
+                                return $ IntVal (i1 + i2)
+                              _ -> throwError "type error in additon"
+eval2 env (Abs n e) = return $ FunVal env n e
+eval2 env (App e1 e2) = do val1 <- eval2 env e1
+                           val2 <- eval2 env e2
+                           case val1 of
+                             FunVal env' n body ->
+                               eval2 (Map.insert n val2 env') body
+                             _ -> throwError "type error in application"
+                           
