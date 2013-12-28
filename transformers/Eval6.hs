3251a20e7337020f67e7c3d26f8824e0ce1528c3
Sibi <sibi@psibi.in>
2013-12-28 18:20:43 +0530

    Eval6.hs

--- transformers/Eval6.hs
+++ transformers/Eval6.hs
@@ -5,15 +5,15 @@ import Control.Monad.State
 import Control.Monad.Writer
 import qualified Data.Map as Map
 
-type Eval5 a = ReaderT Env (ErrorT String
-                           (WriterT [String] (StateT Integer Identity))) a
+type Eval6 a = ReaderT Env (ErrorT String
+                           (WriterT [String] (StateT Integer IO))) a
 
 tick :: (Num s, MonadState s m) => m ()
 tick = do st <- get
           put (st + 1)
 
-runEval5 :: Env -> Integer -> Eval5 a -> ((Either String a, [String]), Integer)
-runEval5 env st ev = runIdentity (runStateT (runWriterT (runErrorT (runReaderT ev env))) st)
+runEval6 :: Env -> Integer -> Eval6 a -> IO ((Either String a, [String]), Integer)
+runEval6 env st ev = runStateT (runWriterT (runErrorT (runReaderT ev env))) st
 
 type Name = String
 data Exp = Lit Integer
@@ -29,32 +29,31 @@ data Value = IntVal Integer
 
 type Env = Map.Map Name Value
 
-eval5 :: Exp -> Eval5 Value
-eval5 (Lit i) = do tick
+eval6 :: Exp -> Eval6 Value
+eval6 (Lit i) = do tick
                    return $ IntVal i
-eval5 (Var n) = do tick
+eval6 (Var n) = do tick
                    env <- ask
                    tell [n]
                    case Map.lookup n env of
                      Nothing -> throwError ("undefined variable: " ++ n)
                      Just x -> return x
-eval5 (Plus e1 e2) = do tick
+eval6 (Plus e1 e2) = do tick
                         env <- ask
-                        e1' <- eval5 e1
-                        e2' <- eval5 e2
+                        e1' <- eval6 e1
+                        e2' <- eval6 e2
                         case (e1', e2') of
                           (IntVal i1, IntVal i2) ->
                               return $ IntVal (i1 + i2)
                           _ -> throwError "type error in addition"
-eval5 (Abs n e) = do tick
+eval6 (Abs n e) = do tick
                      env <- ask
                      return $ FunVal env n e 
-eval5 (App e1 e2) = do tick
-                       val1 <- eval5 e1
-                       val2 <- eval5 e2
+eval6 (App e1 e2) = do tick
+                       val1 <- eval6 e1
+                       val2 <- eval6 e2
                        case val1 of
                          FunVal env' n body ->
                              local (const (Map.insert n val2 env'))
-                                 (eval5 body)
+                                 (eval6 body)
                          _ -> throwError "type error in application"
-
