fe715547e65be50646bd0157c6831eeb40aa2126
Sibi <sibi@psibi.in>
2013-12-31 13:37:29 +0530

    sequence_ function.

--- all_about_monads/Monad.hs
+++ all_about_monads/Monad.hs
@@ -10,3 +10,6 @@ instance Monad Maybe where
 sequence :: Monad m => [m a] -> m [a]
 sequence = foldr mcons (return [])
   where mcons p q = p >>= \x -> q >>= \y -> return (x:y)
+
+sequence_ :: Monad m => [m a] -> m ()
+sequence_ = foldr (>>) (return ())
