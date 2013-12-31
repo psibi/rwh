6c5c66ebf8e4596b70c5091b19b06f7b9fe7521c
Sibi <sibi@psibi.in>
2013-12-31 13:49:37 +0530

    Mapping function.

--- all_about_monads/Monad.hs
+++ all_about_monads/Monad.hs
@@ -7,9 +7,20 @@ instance Monad Maybe where
     (Just x) >>= f  = f x
     return x        = Just x
 
+--- Sequence Functions
+
 sequence :: Monad m => [m a] -> m [a]
 sequence = foldr mcons (return [])
   where mcons p q = p >>= \x -> q >>= \y -> return (x:y)
 
 sequence_ :: Monad m => [m a] -> m ()
 sequence_ = foldr (>>) (return ())
+
+
+--- Mapping functions
+
+mapM :: Monad m => (a -> m b) -> [a] -> m [b]
+mapM f as = sequence $ map f as
+
+mapM_ :: Monad m => (a -> m b) -> [a] -> m ()
+mapM_ f as = sequence_ $ map f as 
