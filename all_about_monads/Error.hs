b599f42a87f0b1eee95e26582e215682c1baa999
Sibi <sibi@psibi.in>
2014-01-05 16:39:52 +0530

    Error Typeclass added.

--- all_about_monads/Error.hs
+++ all_about_monads/Error.hs
@@ -1,8 +1,20 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE FlexibleInstances #-}
+
+class Error a where
+    noMsg :: a
+    strMsg :: String -> a
+
 class (Monad m) => MonadError e m | m -> e where
     throwError :: e -> m a
     catchError :: m a -> (e -> m a) -> m a
 
-instance MonadError (Either e) where
-    throwError = Left
-    (Left e) `catchError` handler = handler e
-    a `catchError` _ = a
+instance Error e => MonadError e (Either e) where
+    throwError = Left 
+    (Left e) `catchError` handler = handler e 
+    a        `catchError` _       = a
+
+--- Example
+
+data ParseError = ParseError {location :: Int, reason :: String}
