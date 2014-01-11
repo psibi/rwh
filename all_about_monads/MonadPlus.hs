93aed3a1c061e84987469e6ccee5adf9f205833d
Sibi <sibi@psibi.in>
2014-01-11 17:56:23 +0530

    Added examples.

--- all_about_monads/MonadPlus.hs
+++ all_about_monads/MonadPlus.hs
@@ -16,3 +16,18 @@ msum xs = foldr mplus mzero xs
 
 guard :: MonadPlus m => Bool -> m ()
 guard p = if p then return () else mzero
+
+-- Some Examples
+
+type Variable = String
+type Value = String
+type EnvironmentStack = [[(Variable, Value)]]
+
+lookupVar1 :: Variable -> EnvironmentStack -> Maybe Value
+lookupVar1 var [] = Nothing
+lookupVar1 var (x:xs) = let val = lookup var x
+                        in maybe (lookupVar1 var xs) Just val
+
+-- Using 
+lookupVar2 :: Variable -> EnvironmentStack -> Maybe Value
+lookupVar2 var stack = msum $ map (lookup var) stack
