764e54b3442a0b022ae6bc51ca04da37e12246cf
Sibi <sibi@psibi.in>
2014-03-02 14:43:29 +0530

    waitFor function.

--- multicore/NiceFork.hs
+++ multicore/NiceFork.hs
@@ -29,6 +29,18 @@ getStatus (Mgr mgr) tid =
       Just st -> tryTakeMVar st >>= \mst -> case mst of
         Nothing -> return (m, Just Running)
         Just sth -> return (M.delete tid m, Just sth)
+
+waitFor :: ThreadManager -> ThreadId -> IO (Maybe ThreadStatus)
+waitFor (Mgr mgr) tid = do
+  maybeDone <- modifyMVar mgr $ \m ->
+    return $ case M.updateLookupWithKey (\_ _ -> Nothing) tid m of
+      (Nothing, _) -> (m, Nothing)
+      (done, m') -> (m', done)
+  case maybeDone of
+    Nothing -> return Nothing
+    Just st -> Just `fmap` takeMVar st
+    
+
   
                           
 
