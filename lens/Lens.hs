8b285b8101bf115a0783ecfa896dd0a382e68f08
Sibi <sibi@psibi.in>
2014-03-18 02:00:55 +0530

    Lens
    Uses Edward Kmett's Lens library package.

---
+++ lens/Lens.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+-- https://www.fpcomplete.com/user/tel/basic-lensing
+-- Not exactly follows the article, slightly different.
+
+import Control.Lens
+
+data Arc = Arc { _degree :: Int, _minute :: Int, _second :: Int }
+data Location = Location { _latitude :: Arc, _longitude :: Arc}
+
+makeLenses ''Location
+
+getLatitude :: Location -> Arc
+getLatitude loc = view latitude loc
+
+setLatitude :: Arc -> Location -> Location
+setLatitude arc loc = set latitude arc loc
+
+getLatitudeR :: Location -> Arc
+getLatitudeR (Location { _latitude = lat }) = lat
+
+setLatitudeR :: Arc -> Location -> Location
+setLatitudeR ac loc = loc { _latitude = ac }
+
+modifyLatitude :: (Arc -> Arc) -> (Location -> Location)
+modifyLatitude f = latitude `over` f
+
+modifyLatitude2 :: (Arc -> Arc) -> Location -> Location
+modifyLatitude2 f lat = setLatitude (f $ getLatitude lat) lat
