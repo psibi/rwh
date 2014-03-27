d0423964a14302b93f17b29caeb8a786ffd085ed
Sibi <sibi@psibi.in>
2014-03-27 14:11:17 +0530

    Parses Positive float and also skips first line.

--- parser/Example.hs
+++ parser/Example.hs
@@ -2,20 +2,28 @@ import Text.ParserCombinators.Parsec
 import Control.Applicative hiding ((<|>))
 import Control.Monad
 
-data Test = Test Integer Integer deriving Show
+data Test = Test Integer Double deriving Show
 
 integer :: Parser Integer
 integer = rd <$> many1 digit
     where rd = read :: String -> Integer
+
+positiveFloat :: Parser Double
+positiveFloat = do
+  a <- many1 digit
+  char '.'
+  b <- many1 digit
+  return $ rd (a ++ "." ++ b)
+    where rd = read :: String -> Double
           
 testParser :: Parser Test
 testParser = do
   a <- integer
   char ','
-  b <- integer
+  b <- positiveFloat
   return $ Test a b
 
-testParserFile = endBy testParser eol
+testParserFile = manyTill anyChar newline *> endBy testParser eol
   
 eol :: Parser Char
 eol = char '\n'
