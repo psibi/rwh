b4540ae37dc6b0f3b7a513837fa9aea06c29e866
Sibi <sibi@psibi.in>
2014-03-31 06:37:27 +0530

    main added.

--- multicore/SortMain.hs
+++ multicore/SortMain.hs
@@ -2,7 +2,7 @@ module Main where
 
 import Data.Time.Clock (diffUTCTime, getCurrentTime)
 import System.Environment (getArgs)
-import System.Random (StdGen, getStdGen, randoms)
+import System.Random (StdGen, getStdGen, randoms, mkStdGen)
 
 import Sorting
 
@@ -13,3 +13,15 @@ testFunction = parSort
 randomInts :: Int -> StdGen -> [Int]
 randomInts k g = let result = take k (randoms g)
                  in force result `seq` result
+
+main = do
+  args <- getArgs
+  let count | null args = 500000
+            | otherwise = read (head args)
+  let input = randomInts count (mkStdGen 3000)
+  putStrLn $ "We have " ++ show (length input) ++ " elements to sort."
+  start <- getCurrentTime
+  let sorted = testFunction input
+  putStrLn $ "Sorted all " ++ show (length sorted) ++ " elements."
+  end <- getCurrentTime
+  putStrLn $ show (end `diffUTCTime` start) ++ " elapsed."
