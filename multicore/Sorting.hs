4df3f47f06dd3fe3a08e415c210cd828e46f3b37
Sibi <sibi@psibi.in>
2014-04-07 01:08:11 +0530

    parSort2

--- multicore/Sorting.hs
+++ multicore/Sorting.hs
@@ -9,10 +9,21 @@ sort (x:xs) = lesser ++ x:greater
 sort _ = []
 
 parSort :: (Ord a) => [a] -> [a]
+parSort [] = []
 parSort (x:xs) = force greater `par` (force lesser `pseq`
                                       (lesser ++ x:greater))
   where lesser = parSort [y | y <- xs, y < x]
         greater = parSort [y | y <- xs, y >= x]
+
+parSort2 :: (Ord a) => Int -> [a] -> [a]
+parSort2 d list@(x:xs)
+  | d <= 0 = sort list
+  | otherwise = force greater `par` (force lesser `pseq`
+                                    (lesser ++  x:greater))
+  where lesser = parSort2 d' [y | y <- xs, y < x]
+        greater = parSort2 d' [y | y <- xs, y >=x]
+        d' = d - 1
+parSort2 _ _ = []
 
 force :: [a] -> ()
 force xs = go xs `pseq` ()
