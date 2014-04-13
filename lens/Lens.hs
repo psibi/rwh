4a68c73a2079d39ef6d15943c18d9d63fb69e266
Sibi <sibi@psibi.in>
2014-04-14 01:18:58 +0530

    Worked out the entire article. Somethings don't typecheck (they are commented).

--- lens/Lens.hs
+++ lens/Lens.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 -- https://www.fpcomplete.com/user/tel/basic-lensing
 -- Not exactly follows the article, slightly different.
@@ -27,3 +28,16 @@ modifyLatitude f = latitude `over` f
 
 modifyLatitude2 :: (Arc -> Arc) -> Location -> Location
 modifyLatitude2 f lat = setLatitude (f $ getLatitude lat) lat
+
+makeLenses ''Arc
+
+getDegreeofLat :: Location -> Int
+getDegreeofLat = view degree . view latitude
+
+setDegreeofLat :: Int -> Location -> Location
+setDegreeofLat = over latitude . set degree
+
+degreeofLat'Manually :: Lens' Location Int
+degreeofLat'Manually = lens getDegreeofLat (flip setDegreeofLat)
+
+-- degreeofLat = latitude . degree
