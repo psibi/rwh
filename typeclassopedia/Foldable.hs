9af7d3ff02e49e77d27316243cb6a1e511b144e1
Sibi <sibi@psibi.in>
2014-04-13 23:35:25 +0530

    foldable.

---
+++ typeclassopedia/Foldable.hs
@@ -0,0 +1,13 @@
+import Data.Monoid
+
+class Foldable t where
+  fold    :: Monoid m => t m -> m
+  foldMap :: Monoid m => (a -> m) -> t a -> m
+
+  foldr   :: (a -> b -> b) -> b -> t a -> b
+  foldl   :: (a -> b -> a) -> a -> t b -> a
+  foldr1  :: (a -> a -> a) -> t a -> a
+  foldl1  :: (a -> a -> a) -> t a -> a
+
+mconcat :: Monoid a => [a] -> a
+so :: [a]
