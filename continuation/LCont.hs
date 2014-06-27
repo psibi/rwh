9e57f83a937ff042cf7ffe5c9762ff9ab84e4618
Sibi <sibi@psibi.in>
2014-06-27 22:54:25 +0530

    Monad magic.

---
+++ continuation/LCont.hs
@@ -0,0 +1,22 @@
+import Control.Monad.Trans.Cont
+
+add :: Int -> Int -> Int
+add x y = x + y
+
+square :: Int -> Int
+square x = x * x
+
+pythagoras :: Int -> Int -> Int
+pythagoras x y = add (square x) (square y)
+
+add_cont :: Int -> Int -> Cont r Int
+add_cont x y = return (add x y)
+
+square_cont :: Int -> Cont r Int
+square_cont x = return (square x)
+
+pythagoras_cont :: Int -> Int -> Cont r Int
+pythagoras_cont x y = do
+  a <- square_cont x
+  b <- square_cont y
+  add_cont a b
