1e44a5559fad0e01ed556591b65a3d54e6204573
Sibi <sibi@psibi.in>
2014-07-04 00:09:16 +0530

    More birds

--- bird/1/Soln.hs
+++ bird/1/Soln.hs
@@ -10,3 +10,19 @@ max :: (Num a,Ord a) => a -> a -> a
 max x y = if (x > y)
           then x
           else y
+
+-- 1.1.3
+area :: Fractional a => a -> a
+area r = pi * (square r)
+  where pi = 22 / 7
+
+-- 1.2.1
+-- Two forms: Call by need and call by value.
+
+-- 1.2.2
+-- Same as above
+
+-- 1.2.3
+-- ( succ (pred ( succ (pred (pred zero)))))
+-- ( succ (pred (pred zero)))
+-- ( pred zero)
