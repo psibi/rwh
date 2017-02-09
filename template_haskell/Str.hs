05f73712f52963cd550f9adc63a5e4cb13f3f370
Sibi Prabakaran <sibi@psibi.in>
2017-02-09 23:26:52 +0530

    Add a more interesting program

--- template_haskell/Str.hs
+++ template_haskell/Str.hs
@@ -1,10 +1,35 @@
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE DeriveLift #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Str where
 
 import Language.Haskell.TH
 import Language.Haskell.TH.Quote
+import Language.Haskell.TH.Syntax
+import Data.Time.Format
+import Data.Time
 
 str :: QuasiQuoter
 str =
   QuasiQuoter
   { quoteExp = stringE
   }
+
+deriving instance Lift Day
+
+instance Lift DiffTime where
+  lift x = return $ LitE (IntegerL $ diffTimeToPicoseconds x)
+
+deriving instance Lift UTCTime
+
+time :: QuasiQuoter
+time =
+  QuasiQuoter
+  { quoteExp =
+    \str ->
+       let (item :: Maybe UTCTime) = parseTimeM True defaultTimeLocale "%Y" str
+       in [|item|]
+  }
+-- instance Lift UTCTime
--- template_haskell/temp.hs
+++ template_haskell/temp.hs
@@ -1,6 +1,6 @@
 #!/usr/bin/env stack
 {- stack
-     --resolver lts-6.15
+     --resolver lts-7.9
      --install-ghc
      runghc
      --package shakespeare
@@ -9,9 +9,15 @@
 {-#LANGUAGE QuasiQuotes#-}
 
 import Str
+import Data.Time
 
 lng :: String
 lng = [str|hello world|]
 
+time2 :: Maybe UTCTime
+time2 = [time|2017|]
+
 main :: IO ()
-main = print lng
+main = do
+  print lng
+  print time2
