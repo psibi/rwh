da7844154dfa25b31db6f18eacdb7840d454b25a
Sibi Prabakaran <sibi@psibi.in>
2017-12-18 00:08:57 +0530

    Optional option

---
+++ optparse/optional_option.hs
@@ -0,0 +1,56 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Main where
+
+-- import Data.Semigroup ((<>))
+-- usage: stack nested_command_option.hs -- -help
+import Options.Applicative
+
+data Opts = Opts
+  { optGlobalFlag :: !Bool
+  , optCommand :: !Command
+  }
+
+data CreateOpts = CreateOpts
+  { coptsFlag :: Bool
+  , coptsHelpFlag :: Bool
+  , coptsCommand :: String
+  } deriving (Show, Eq, Ord)
+
+data Command
+  = Create CreateOpts
+  | Delete
+
+main :: IO ()
+main = do
+  (opts :: Opts) <- execParser optsParser
+  case optCommand opts of
+    Create name -> putStrLn ("Created the thing named " ++ (show name))
+    Delete -> putStrLn "Deleted the thing!"
+  putStrLn ("global flag: " ++ show (optGlobalFlag opts))
+  where
+    optsParser :: ParserInfo Opts
+    optsParser =
+      info
+        (helper <*> versionOption <*> programOptions)
+        (fullDesc <> progDesc "optparse subcommands example" <>
+         header
+           "optparse-sub-example - a small example program for optparse-applicative with subcommands")
+    versionOption :: Parser (a -> a)
+    versionOption = infoOption "0.0" (long "version" <> help "Show version")
+    programOptions :: Parser Opts
+    programOptions =
+      Opts <$> switch (long "global-flag" <> help "Set a global flag") <*>
+      (hsubparser ((createCommand <> deleteCommand)) <|> pure Delete)
+    createCommand :: Mod CommandFields Command
+    createCommand = command "create" (info copt (progDesc "Create a thing"))
+    deleteCommand :: Mod CommandFields Command
+    deleteCommand =
+      command "delete" (info (pure Delete) (progDesc "Delete the thing"))
+    createOptions :: Parser CreateOpts
+    createOptions =
+      CreateOpts <$> switch (long "some-flag" <> help "Set the flag") <*>
+      switch (long "ome-flag" <> help "Set the flag") <*>
+      strArgument (metavar "NAME" <> help "Name of the thing to create")
+    copt :: Parser Command
+    copt = fmap (Create) createOptions
