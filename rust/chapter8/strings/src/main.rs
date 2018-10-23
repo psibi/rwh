b1b133e85458a37e5f29152e45d0d2b422316ec8
Sibi Prabakaran <sibi@psibi.in>
2018-10-23 23:11:28 +0530

    Chapter 8

---
+++ rust/chapter8/strings/src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    let s = "hllo".to_string();
+    println!("Hello, world! {}", s);
+}
--- rust/chapter8/vectors/src/main.rs
+++ rust/chapter8/vectors/src/main.rs
@@ -1,6 +1,27 @@
 fn main() {
-    
-    let v = vec![1,2,3,4,5]
-    
-    println!("Hello, world!");
+    // let mut v = vec![1, 2, 3, 4, 5];
+    // let third: &i32 = &v[2];
+    // v.push(7);
+
+    let mut v = vec![1, 2, 3, 4, 5];
+    for i in &mut v {
+        *i = *i + 50;
+    }
+    for i in &v {
+        println!("{}", i);
+    }
+
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Float(10.12),
+        SpreadsheetCell::Text(String::from("blue")),
+    ];
+
+    // println!("Hello, world!, {}", third);
 }
