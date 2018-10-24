5e2f931998484208b9005a1860f4fcc9e1279de5
Sibi Prabakaran <sibi@psibi.in>
2018-10-24 10:54:20 +0530

    Update

--- rust/chapter8/hashmap/src/main.rs
+++ rust/chapter8/hashmap/src/main.rs
@@ -1,5 +1,9 @@
 use std::collections::HashMap;
 
 fn main() {
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 50);
+    scores.insert(String::from("Green"), 60);
+
     println!("Hello, world!");
 }
