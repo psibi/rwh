19cb6025d3e6d9e60d5c39c5f7bd2829ecd0d587
Sibi Prabakaran <sibi@psibi.in>
2019-02-10 01:21:50 +0530

    Add other examples

--- template_haskell/sample/src/Lib.hs
+++ template_haskell/sample/src/Lib.hs
@@ -23,6 +23,7 @@ genSampleInt = do
   let si = mkName "sampleInt"
   return $
     [SigD si (ConT ''Int), ValD (VarP si) (NormalB (LitE (IntegerL 3))) []]
+
 -- That's all it takes. See the 'Test.hs' module in which the code is generated.
 -- Let's test that in ghci:
 -- λ> import Test
@@ -30,3 +31,47 @@ genSampleInt = do
 -- sampleInt :: Int
 -- λ> sampleInt
 -- 3
+-- Another way to do the same stuff
+genSampleInt2 :: Q [Dec]
+genSampleInt2 =
+  [d|
+
+  sampleInt2 :: Int
+  sampleInt2 = 4 |]
+
+-- Can we pass parameter to the functions ? Let's check that out
+genSampleInt3 :: Int -> Q [Dec]
+genSampleInt3 x =
+  [d|
+
+  sampleInt3 :: Int
+  sampleInt3 = x |]
+
+-- And that works fine. Ok, now let's move to more complex stuff. Can we pass it a function?
+-- Let's define some function
+dummyFunction :: Int -> Int
+dummyFunction x = x * 100
+
+-- Now let's try to use that dummyFunction in the generator function:
+genSampleInt4 :: Int -> Q [Dec]
+genSampleInt4 x =
+  [d|
+
+  sampleInt4 :: Int
+  sampleInt4 = if (x > 3) then x * 5 else dummyFunction x |]
+
+-- That also works. Cool!
+-- Now let's try to see if you can pass a combinator to the generator function.
+-- This code doesn't work:
+-- genSampleInt5 :: (Int -> Int) -> Int -> Q [Dec]
+-- genSampleInt5 comb x =
+--   [d|
+--   sampleInt5 :: Int
+--   sampleInt5 = comb x |]
+genSampleInt5 :: (Int -> Int) -> Int -> Q [Dec]
+genSampleInt5 comb x =
+  let f = comb x
+   in [d|
+
+  sampleInt5 :: Int
+  sampleInt5 = f |]
--- template_haskell/sample/src/Test.hs
+++ template_haskell/sample/src/Test.hs
@@ -5,3 +5,11 @@ module Test where
 import Lib
 
 $(genSampleInt)
+
+$(genSampleInt2)
+
+$(genSampleInt3 8)
+
+$(genSampleInt4 2)
+
+$(genSampleInt5 (\x -> 99) 4)
